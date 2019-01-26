4d98a2af4e0be3323e55c07db0e2f83de74622bd
Lexi <ambellinger24@gmail.com>
2019-01-26 14:02:05 -0600

    added jaws

--- Solved/app/config/connection.js
+++ Solved/app/config/connection.js
@@ -1,21 +1,40 @@
-// *********************************************************************************
-// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
-// *********************************************************************************
+// // *********************************************************************************
+// // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// // *********************************************************************************
 
-// Dependencies
-var Sequelize = require("sequelize");
+// // Dependencies
+// var Sequelize = require("sequelize");
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("sequelize_library", "root", "", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+// // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+// var sequelize = new Sequelize("sequelize_library", "root", "", {
+//   host: "localhost",
+//   port: 3306,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   }
+// });
 
-// Exports the connection for other files to use
-module.exports = sequelize;
+
+
+
+// // Exports the connection for other files to use
+// module.exports = sequelize;
+
+var mysql = require("mysql");
+
+var connection;
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "alexisbellinger",
+    password: "DNB63088",
+    database: "burgerDB"
+  })
+}
